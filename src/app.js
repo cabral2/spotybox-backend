7e80b84a383112ad91f85ffc679d6bd0b1f15b63
cabral2 <saviocampolina9@gmail.com>
2022-07-10 18:16:19 -0300

    Enable cors

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ const app = express();
 // Set body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
 
 //Load Routes
 const healthcheck = require('./routes/healthcheck');
