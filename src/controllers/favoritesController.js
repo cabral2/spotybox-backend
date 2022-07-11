4446690ba0e6d380cc0b82dd5294c655a7b80f0e
Iury Zanonni <izanonni@gmail.com>
2022-07-11 02:41:19 -0300

    Create route favorite

--- src/controllers/favoritesController.js
+++ src/controllers/favoritesController.js
@@ -1,50 +1,55 @@
 const repository = require('../repositories/favoritesRepository');
 
 exports.getByUser = async (req, res, next) => {
-    const albuns = await repository.getFavoritesByUser(req.query.id);
-    res.status(200).send(albuns);
-}
+  const albuns = await repository.getFavoritesByUser(req.query.id);
+  res.status(200).send(albuns);
+};
+
+exports.getByUserAndAlbum = async (req, res, next) => {
+  const user = await repository.getFavoritesByUserAndAlbum(req.query);
+  res.status(200).send(user[0]);
+};
 
 exports.post = async (req, res, next) => {
-    if (!req.body.userId || !req.body.albumId) {
-        res.status(404).send({
-            message: 'Solicitação inválida',
-        });
-        return;
-    }
+  if (!req.body.userId || !req.body.albumId) {
+    res.status(404).send({
+      message: 'Solicitação inválida',
+    });
+    return;
+  }
 
-    const favorite = {};
-    favorite.user_id = req.body.userId;
-    favorite.album_id = req.body.albumId;
+  const favorite = {};
+  favorite.user_id = req.body.userId;
+  favorite.album_id = req.body.albumId;
 
-    try {
-        await repository.saveFavorite(favorite);
-        res.status(201).send(favorite);
-    } catch (error) {
-        res.status(500).send({
-            message: 'Falha ao criar favorite',
-        });
-    }
+  try {
+    await repository.saveFavorite(favorite);
+    res.status(201).send(favorite);
+  } catch (error) {
+    res.status(500).send({
+      message: 'Falha ao criar favorite',
+    });
+  }
 };
 
 exports.delete = async (req, res, next) => {
-    if (!req.body.userId || !req.body.albumId) {
-        res.status(404).send({
-            message: 'Solicitação inválida',
-        });
-        return;
-    }
+  if (!req.body.userId || !req.body.albumId) {
+    res.status(404).send({
+      message: 'Solicitação inválida',
+    });
+    return;
+  }
 
-    const favorite = {};
-    favorite.user_id = req.body.userId;
-    favorite.album_id = req.body.albumId;
+  const favorite = {};
+  favorite.user_id = req.body.userId;
+  favorite.album_id = req.body.albumId;
 
-    try {
-        await repository.deleteFavorite(favorite);
-        res.status(200).send(favorite);
-    } catch (error) {
-        res.status(500).send({
-            message: 'Falha ao deletar favorite',
-        });
-    }
+  try {
+    await repository.deleteFavorite(favorite);
+    res.status(200).send(favorite);
+  } catch (error) {
+    res.status(500).send({
+      message: 'Falha ao deletar favorite',
+    });
+  }
 };
--- src/routes/favoritesRoute.js
+++ src/routes/favoritesRoute.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const controller = require('../controllers/favoritesController');
 
 router.get('/user', controller.getByUser);
+router.get('/user_favorite', controller.getByUserAndAlbum);
 router.post('/', controller.post);
 router.delete('/', controller.delete);
 
