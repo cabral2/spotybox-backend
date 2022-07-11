da9c7366d5c9de281e2f5e2e07667aa3f9a7181a
Davi Emediato <48991554+daviemediato@users.noreply.github.com>
2022-07-10 23:14:57 -0300

    creating get by email user route

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const controller = require('../controllers/userController');
 
+router.get('/email', controller.getByEmail);
 router.post('/create', controller.post);
 
 module.exports = router;
